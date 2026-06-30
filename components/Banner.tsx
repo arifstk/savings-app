ea96b10f000e6ecc611ec8a1c4a795b77cc39546
arifstk <arifsk.dev@gmail.com>
2026-06-30 22:54:46 +0600

    banner edit

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -31,11 +31,11 @@ export default function Banner() {
   if (!banner.h1 && !banner.h2 && !banner.p) return null;
 
   return (
-    <div className="w-full bg-linear-to-r from-teal-500 to-cyan-500 px-6 py-10 text-center rounded-xl">
+    <div className="w-full min-h-[70vh] bg-linear-to-r from-teal-500 to-cyan-500 px-6 py-10 text-center items-center rounded-xl">
       <div className="max-w-3xl mx-auto">
         {banner.h1 && (
           <h1
-            className={`text-3xl sm:text-4xl font-bold mb-2 text-white ${isBangla(banner.h1) ? banglaFont.className : ""
+            className={`text-3xl sm:text-5xl font-bold mb-2 text-white text-shadow-gray-700 ${isBangla(banner.h1) ? banglaFont.className : ""
               }`}
           >
             {banner.h1}
@@ -43,7 +43,7 @@ export default function Banner() {
         )}
         {banner.h2 && (
           <h2
-            className={`text-xl sm:text-2xl font-semibold text-teal-800 mb-3 ${isBangla(banner.h2) ? banglaFont.className : ""
+            className={`text-xl sm:text-3xl font-semibold text-white text-shadow-gray-700 mb-3 ${isBangla(banner.h2) ? banglaFont.className : ""
               }`}
           >
             {banner.h2}
@@ -51,7 +51,7 @@ export default function Banner() {
         )}
         {banner.p && (
           <p
-            className={`mx-auto max-w-xl text-base text-teal-900/80 ${isBangla(banner.p) ? banglaFont.className : ""
+            className={`mx-auto max-w-xl text-base text-white text-shadow-gray-700 ${isBangla(banner.p) ? banglaFont.className : ""
               }`}
           >
             {banner.p}
