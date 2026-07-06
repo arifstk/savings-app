79bbbd4a8416838794e89bfeca26b50407993aa0
arifstk <arifsk.dev@gmail.com>
2026-07-06 22:08:31 +0600

    banner protected from public

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -2,6 +2,7 @@
 
 import { useEffect, useState } from "react";
 import { Tiro_Bangla } from "next/font/google";
+import { useSession } from "next-auth/react";
 
 const banglaFont = Tiro_Bangla({
   subsets: ["bengali"],
@@ -19,6 +20,8 @@ const isBangla = (text: string) => /[\u0980-\u09FF]/.test(text);
 
 export default function Banner() {
   const [banner, setBanner] = useState<BannerData | null>(null);
+  const { data: session } = useSession();
+  const role = session?.user?.role ?? null;
 
   useEffect(() => {
     fetch("/api/admin/banner")
@@ -31,33 +34,41 @@ export default function Banner() {
   if (!banner.h1 && !banner.h2 && !banner.p) return null;
 
   return (
-    <div className="w-full min-h-[60vh] bg-linear-to-r from-teal-500 to-cyan-500 px-6 py-10 text-center items-center rounded-xl">
-      <div className="max-w-3xl mx-auto">
-        {banner.h1 && (
-          <h1
-            className={`text-3xl sm:text-5xl font-bold mb-2 text-white text-shadow-gray-700 ${isBangla(banner.h1) ? banglaFont.className : ""
-              }`}
-          >
-            {banner.h1}
-          </h1>
-        )}
-        {banner.h2 && (
-          <h2
-            className={`text-xl sm:text-3xl font-semibold text-white text-shadow-gray-700 mb-3 ${isBangla(banner.h2) ? banglaFont.className : ""
-              }`}
-          >
-            {banner.h2}
-          </h2>
-        )}
-        {banner.p && (
-          <p
-            className={`mx-auto max-w-xl text-base text-white text-shadow-gray-700 ${isBangla(banner.p) ? banglaFont.className : ""
-              }`}
-          >
-            {banner.p}
-          </p>
-        )}
-      </div>
+    <div>
+      {(role === "user" || role === "admin") && (
+        <div className="w-full min-h-[60vh] bg-linear-to-r from-teal-500 to-cyan-500 px-6 py-10 text-center items-center rounded-xl">
+          <div className="max-w-3xl mx-auto">
+            {banner.h1 && (
+              <h1
+                className={`text-3xl sm:text-5xl font-bold mb-2 text-white text-shadow-gray-700 ${isBangla(banner.h1) ? banglaFont.className : ""
+                  }`}
+              >
+                {banner.h1}
+              </h1>
+            )}
+            {banner.h2 && (
+              <h2
+                className={`text-xl sm:text-3xl font-semibold text-white text-shadow-gray-700 mb-3 ${isBangla(banner.h2) ? banglaFont.className : ""
+                  }`}
+              >
+                {banner.h2}
+              </h2>
+            )}
+            {banner.p && (
+              <p
+                className={`mx-auto max-w-xl text-base text-white text-shadow-gray-700 ${isBangla(banner.p) ? banglaFont.className : ""
+                  }`}
+              >
+                {banner.p}
+              </p>
+            )}
+          </div>
+        </div>
+      )}
     </div>
+
+
+
   );
 }
+
